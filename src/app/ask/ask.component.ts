c747a9aa9e3b132c16635283901224e9052e7bcf
Chinh Nguyen <cqnguyen21st@yahoo.com>
2018-12-08 14:51:07 -0800

    got camera working

--- src/app/ask/ask.component.ts
+++ src/app/ask/ask.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-ask',
   templateUrl: './ask.component.html',
   styleUrls: ['./ask.component.scss']
 })
-export class AskComponent implements OnInit {
-
+export class AskComponent implements OnInit, AfterViewInit {
+  @ViewChild('videoElement') videoElement: ElementRef;  
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    var browser = navigator;
+
+    browser.getUserMedia = (browser.getUserMedia);
+
+    const video = this.videoElement.nativeElement;
+    const config = { video: true, audio: false };
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    });
+  }
 }
