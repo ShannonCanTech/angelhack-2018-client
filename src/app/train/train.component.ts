84d373282163b6549a277c5376b153e1d8034067
Chinh Nguyen <cqnguyen21st@yahoo.com>
2018-12-08 18:41:20 -0800

    train button to start training

--- src/app/train/train.component.ts
+++ src/app/train/train.component.ts
@@ -24,6 +24,14 @@ export class TrainComponent implements OnInit, AfterViewInit {
 
   trainDataInterval: NodeJS.Timer;
 
+  model: any;
+
+  // Learning config
+  learningRate: number = 0.0001;
+  denseUnit: number = 100;
+  batchSizeFraction: number = 0.4;
+  epoches: number = 20;
+
   private video;
 
   constructor(private webcamService: WebcamService, private controllerDatasetService: ControllerDatasetService) { }
@@ -46,11 +54,75 @@ export class TrainComponent implements OnInit, AfterViewInit {
 
   mousedown() {
     if (!this.trainDataInterval) {
-      this.startTraining();
+      this.addData();
     }
   }
 
-  startTraining() {
+  train() {
+    if (this.controllerDatasetService.xs == null) {
+      throw new Error('Add some examples before training!');
+    }
+  
+    // Creates a 2-layer fully connected model. By creating a separate model,
+    // rather than adding layers to the mobilenet model, we "freeze" the weights
+    // of the mobilenet model, and only train weights from the new model.
+    this.model = tf.sequential({
+      layers: [
+        // Flattens the input to a vector so we can use it in a dense layer. While
+        // technically a layer, this only performs a reshape (and has no training
+        // parameters).
+        tf.layers.flatten({
+          inputShape: this.truncatedMobileNet.outputs[0].shape.slice(1)
+        }),
+        // Layer 1.
+        tf.layers.dense({
+          units: this.denseUnit,
+          activation: 'relu',
+          kernelInitializer: 'varianceScaling',
+          useBias: true
+        }),
+        // Layer 2. The number of units of the last layer should correspond
+        // to the number of classes we want to predict.
+        tf.layers.dense({
+          units: this.controllerDatasetService.numberOfClasses,
+          kernelInitializer: 'varianceScaling',
+          useBias: false,
+          activation: 'softmax'
+        })
+      ]
+    });
+  
+    // Creates the optimizers which drives training of the model.
+    const optimizer = tf.train.adam(this.learningRate);
+    // We use categoricalCrossentropy which is the loss function we use for
+    // categorical classification which measures the error between our predicted
+    // probability distribution over classes (probability that an input is of each
+    // class), versus the label (100% probability in the true class)>
+    this.model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
+  
+    // We parameterize batch size as a fraction of the entire dataset because the
+    // number of examples that are collected depends on how many examples the user
+    // collects. This allows us to have a flexible batch size.
+    const batchSize =
+        Math.floor(this.controllerDatasetService.xs.shape[0] * this.batchSizeFraction);
+    if (!(batchSize > 0)) {
+      throw new Error(
+          `Batch size is 0 or NaN. Please choose a non-zero fraction.`);
+    }
+  
+    // Train the model! Model.fit() will shuffle xs & ys so we don't have to.
+    this.model.fit(this.controllerDatasetService.xs, this.controllerDatasetService.ys, {
+      batchSize,
+      epochs: this.epoches,
+      callbacks: {
+        onBatchEnd: async (batch, logs) => {
+          console.log('Loss: ' + logs.loss.toFixed(5));
+        }
+      }
+    });
+  }
+
+  private addData() {
     this.trainDataInterval = setInterval(() => {
       tf.tidy(() => {
         const img = this.webcamService.capture(this.video);
