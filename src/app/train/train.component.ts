5fc9d4ae8b94ae329c9cfba24d2505df736f5d86
Chinh Nguyen <cqnguyen21st@yahoo.com>
2018-12-08 17:30:37 -0800

    be able to capture image

--- src/app/train/train.component.ts
+++ src/app/train/train.component.ts
@@ -1,3 +1,4 @@
+import { WebcamService } from './../shared/services/webcam.service';
 import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
@@ -6,24 +7,45 @@ import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angula
   styleUrls: ['./train.component.scss']
 })
 export class TrainComponent implements OnInit, AfterViewInit {
-  @ViewChild('videoElement') videoElement: ElementRef;  
+  @ViewChild('videoElement') videoElement: ElementRef;
+  @ViewChild('sampleElement') sampleElement: ElementRef; 
+
+  videoWidth: number = 224;
+  videoHeight: number = 224;
+
+  private video;
   
-  constructor() { }
+  constructor(private webcamService: WebcamService) { }
 
   ngOnInit() {
+    this.video = this.videoElement.nativeElement;
   }
 
   ngAfterViewInit() {
-    var browser = navigator;
+    this.webcamService.setup(this.video);
+  }
 
-    browser.getUserMedia = (browser.getUserMedia);
+  snapTrain() {
+    const img = this.webcamService.capture(this.video);
+    
+    this.draw(img, this.sampleElement.nativeElement);
+    // this.draw(this.video, this.sampleElement.nativeElement);
 
-    const video = this.videoElement.nativeElement;
-    const config = { video: true, audio: false };
+    console.log('Draw');
+  }
 
-    browser.mediaDevices.getUserMedia(config).then(stream => {
-      video.src = window.URL.createObjectURL(stream);
-      video.play();
-    });
+  private draw(image, canvas) {
+    const [width, height] = [this.videoWidth, this.videoHeight];
+    const ctx = canvas.getContext('2d');
+    const imageData = new ImageData(width, height);
+    const data = image.dataSync();
+    for (let i = 0; i < height * width; ++i) {
+      const j = i * 4;
+      imageData.data[j + 0] = (data[i * 3 + 0] + 1) * 127;
+      imageData.data[j + 1] = (data[i * 3 + 1] + 1) * 127;
+      imageData.data[j + 2] = (data[i * 3 + 2] + 1) * 127;
+      imageData.data[j + 3] = 255;
+    }
+    ctx.putImageData(imageData, 0, 0);
   }
 }
